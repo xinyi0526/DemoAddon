1c376ff9ce8930647ed237abe9be9ddabab7f1f5
xinyi0526 <xinyi.luvand620@gmail.com>
2020-07-23 10:23:38 +0800

    submission

--- app/src/main/java/com/myapplicationdev/android/demoaddon/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demoaddon/MainActivity.java
@@ -3,12 +3,21 @@ package com.myapplicationdev.android.demoaddon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.iv);
+        String imageUrl = "https://i.imgur.com/tGbaZCY.jpg";
+        Picasso.with(this).load(imageUrl).into(iv);
     }
 }
